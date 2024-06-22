c890d086b62d43cbb868614ec36e046a04d2ea28
Sambasivaraobodapati <sambasivaraobodapati177@gmail.com>
2024-06-22 12:17:09 +0530

    Siva has added Day files four new files

---
+++ Day1Ex1.java
@@ -0,0 +1,28 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        /*byte radius = 5;
+        double area = 3.14*radius*radius;
+        double cir = 3.14*2*radius;
+        System.out.println("Area of Circle is " + area + "\nCircumfurence of the Circle is " + cir );*/
+        /*int num1=2;
+        for(int i=1;i<=10;i++)
+        {
+ 	System.out.println(num1 + " * " + i + " = " + (num1 * i)); 
+        }*/
+        /*int len=10,bre=5,area,min=60,per;
+        area = len*bre;
+        per=2*(len+bre);
+        double sec=(float)120/60;
+        float minutes = (float)150/60;
+        System.out.println("Area of rectangele is " + area);
+        System.out.println("Perimeter of rectangle "+ per);
+        System.out.println("10 hours in minutes "+(10*min));
+        System.out.println("5 minutes in seconds "+ 5*min);
+        System.out.println("seconds to minutes "+sec); 
+        System.out.println("Minutes to Hours "+minutes);*/
+        int rupees = 23894 , doller = 83;
+        System.out.println("dollers into rupees "+(rupees*doller));
+        System.out.println("rupees into dollers "+(float)(rupees/doller));
+    }
+}
+    
---
+++ Day2EX1.java
@@ -0,0 +1,20 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        String Name="Samba Siva Rao",Phone_No="6281510571";
+        String DOB="07/06/2002",Address = "1-50A,Dondapadu Near Ramalayam",Email="Samba@gmail.com";
+        String Skills="C,Java,Python",Location="Hyderabad",Qualification="B-TECH",Course="Java Full Stack";
+        int Percentage=75,Exp=0;
+        System.out.println("    HIRING APPLICATION FORM     ");
+        System.out.println("Full Name : " + Name);
+        System.out.println("Phone Number : "+ Phone_No);
+        System.out.println("Date Of Birth : " + DOB);
+        System.out.println("Highest Qualification : "+Qualification);
+        System.out.println("Degree Percentage : "+Percentage+"%");
+        System.out.println("Perment Address : "+Address);
+        System.out.println("Email : "+Email);
+        System.out.println("Skills : "+Skills);
+        System.out.println("Current Location : "+Location);
+        System.out.println("Course Enrollled : "+Course);
+        System.out.println("Experience : "+Exp);
+    }
+}
